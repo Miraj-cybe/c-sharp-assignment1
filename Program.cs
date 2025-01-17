ed4567f68daccb6decfad086085927d2066d0076
Miraj-cybe <mondolm619@gmail.com>
2025-01-18 00:34:11 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿class Assignment_1{
+    public static void Main(string[] args){
+Console.WriteLine( "My Name is miraj mondol");
+Console.WriteLine("I am 21 years old");
+Console.WriteLine("I am Full Stack Web Developer");
+    }
+   
+}
