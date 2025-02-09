5da9d25affa47b94eed772651c60af26a3851776
Miraj-cybe <mondolm619@gmail.com>
2025-02-09 18:20:09 +0600

    Add files via upload
    Here i use the contructor Methood.....every oop[ program language use this methood to code easily .

---
+++ oop Program (constructor).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+class Person{                //creating class
+  public  string value;        //class .. feild
+  public int age;                 //class .. feild
+
+public Person(){
+    Console.WriteLine("I am default .");
+}
+
+public Person( string n , int y){
+   value=n;
+   age=y;
+}
+
+}
+class Program{
+    public static void Main(){
+Person p1= new Person();
+Console.WriteLine($" Name:{p1.value} , Age:{p1.age}");
+
+Person p2= new Person("sajida", 17);
+Console.WriteLine($"Name:{p2.value}, Age:{p2.age}");
+
+
+Person p3= new Person("Kazi Selina", 29);
+Console.WriteLine($" Name:{p3.value} , Age:{p3.age}");
+    }
+}
