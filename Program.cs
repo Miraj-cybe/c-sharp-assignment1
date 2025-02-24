f1f928b1157a8e4d013a10f9e83b1d115578f6a5
Miraj-cybe <mondolm619@gmail.com>
2025-02-24 19:37:45 +0600

    Add files via upload
    Here is a Array for and for each loop code. its my learning purpose

--- Program.cs
+++ Program.cs
@@ -1,8 +1,41 @@
-﻿class Assignment_1{
-    public static void Main(string[] args){
-Console.WriteLine( "My Name is miraj mondol");
-Console.WriteLine("I am 21 years old");
-Console.WriteLine("I am Full Stack Web Developer");
-    }
-   
+﻿using System;
+class Program{
+  public static void Main()
+  {
+//Array number start from [0,1,2,3...]
+
+
+   string[] names ={"BMw", "Bugati" ,"Honda","CHR"};
+Console.WriteLine($"Car:{ names[2]}");
+
+
+//string[] names =new string[4] {"BMw", "Bugati" ,"Honda","CHR"};
+
+ 
+
+
+
+//for loop
+for(int index=0;index<names.Length;index++)
+{
+
+Console.WriteLine($"Car:{ names[index]}");
 }
+
+
+
+//foreach loop
+string[] colors =new string[4] {"Black", "Red" ,"Green","yellow"};
+foreach(string color in colors)
+
+{
+
+Console.WriteLine($"color:{color}");
+
+
+}
+
+
+
+  }
+  }
